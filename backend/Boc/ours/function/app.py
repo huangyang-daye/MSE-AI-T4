ef31f39030b231468c26b7339c9a04ee04836b20
lin32518 <2785833528@qq.com>
2025-06-17 11:53:51 +0800

    Create app.py

---
+++ backend/Boc/ours/function/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import logging
+
+# 导入两个模块
+from init_track import track_bp
+
+# 配置日志
+logging.basicConfig(level=logging.INFO)
+
+# 创建 Flask 应用
+app = Flask(__name__)
+
+# 注册蓝图
+app.register_blueprint(track_bp)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
